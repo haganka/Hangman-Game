ef69dc66e66b9e02fffc5c8d5cf50ad3f5154f67
Kelly Hagan <kellyhagan@Kellys-MacBook-Pro.local>
2018-01-20 15:51:37 -0600

    adds number of lives to page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,73 +7,63 @@
             return campWords[Math.floor(Math.random() * campWords.length)];
         }
         //stores random word chosen
-        var randChoice = randWord(campWords);
+        var randChoice = randWord();
         
-        var wordBank = document.querySelector("#mysteryWord");
-        document.querySelector("#mysteryWord").innerHTML = randChoice;
+        //Adds underscores to page
+        var randChoiceArr = randChoice.split("");
+        var underscoreHTML = "";
+        for (var i = 0; i < randChoiceArr.length; i++){
 
+         //call interestsHTML variable that's currently empty, want to display in list, pull in the interestsArr variable to populate it, close the list
+         underscoreHTML += "_ " ;
+        }
+        var randChoiceElem = document.querySelector("#mysteryWord");
+        document.querySelector("#mysteryWord").innerHTML = underscoreHTML;
+
+        var numGuesses = 7;
+        var numHTML = document.querySelector("#guesses");
+        document.querySelector("#guesses").innerHTML = numGuesses;
+
+
+    // need to figure out how to make the below work as the press any key rather than using a prompt - below is start
+    // var begin = function(event){
+    //     document.addEventListener("keyup", keyPushed);
+    // }
         //prompts user to enter word
         var playerGuess = prompt("Guess a letter");
         var guesses = [];
-        document.querySelector("#lettersDone").innerText = "";
 
-        var remainingGuesses = 7;
-        var guessNum = document.querySelector("#guesses");
-        document.querySelector("#guesses").innerHTML = remainingGuesses;
+
 
         //CJ example in class for how to compare letters to key entered by user
-        var randChoice = "smore"
-        var remainingGuesses = 7;
-        var wordArr = word.split(""); //this is now equal to ['s', 'm', 'o', 'r', 'e'];
+        var randChoice = "smore";
+        var numGuesses = 7;
+        var wordArr = randChoice.split(""); //this is now equal to ['s', 'm', 'o', 'r', 'e'];
         var hasMatch = false;
 
         
         for(var i = 0; i < wordArr.length; i++){
             if (wordArr[i] === playerGuess){
                 hasMatch = true;
-                //this needs to add letter to the display in the right place (create function and call in the if statement) and add a life
+            }
+            if (wordArr[i] === playerGuess){
+                guesses.push(playerGuess)
+                // guesses.push(playerGuess);
+                //above needs to add letter to the display in the right place (create function and call in the if statement) and add a life
             }
         }
 
         if(hasMatch){
-            remainingGuesses++;
+            numGuesses++;
         }else{
-            remainingGuesses--;
+            numGuesses--;
         }
 
         hasMatch = false; 
 
+
         
         //need to find a way to go back to the for loop starting with hasMatch as false
 
-    // need to figure out how to make the below work as the press any key rather than using a prompt
-    var begin = function(event){
-            var keyPress = event.key
-            if (key){
-                console.log("good")
-            }
-        }
 
-
-        // for (var i = 0; i < campWords.length; i--){
-
-        // }
-
-
-
-
-
-        // var campWordsArr = campWords.split();
-        // var campWordsHTML = "";
-        // for (var i = 0; i < campWordsArr.length; i++){
-        //     campWordsHTML += "<li>" +campWordsArr[i] + "</li>";
-        // }
-        // var campWordsElem = document.querySelector("#mysteryWord");
-        // campWordsElem.innerHTML = campWordsHTML;
-
-
-
-
-        
-        // for (var guessesNum = 7; guessesNum > 0; guessesNum--);
       
