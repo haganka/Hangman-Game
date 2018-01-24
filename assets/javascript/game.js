a1485e216637727b13b928bbfd1a75d5b6e45aec
Kelly <kelly.hagan3@gmail.com>
2018-01-24 17:00:07 -0600

    resets game after winand correctly places letters in word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,19 +5,46 @@
         var correctGuesses = [];
         var hasWon = false;
         var guesses = [];
-        var wins = 0;
-       
-        //chooses random word
-        var randWord = function(){
-            return campWords[Math.floor(Math.random() * campWords.length)];
+        var wins = 0;;
+        var wordArr = []
+
+        var guessedWord = []
+
+
+    var randWord = function(){
+        return campWords[Math.floor(Math.random() * campWords.length)];
+    }
+
+    function checkWord(playerGuess) {
+        console.log('guessedWord ---', guessedWord.length, 'wordArr', wordArr.length, 'playerGuess', playerGuess);
+
+        
+        for (var i = 0; i < guessedWord.length; i ++) {
+            console.log('this is wordArr[i]', wordArr[i], 'playerGuess', playerGuess);
+
+            if (playerGuess === wordArr[i]) {
+                guessedWord[i] = playerGuess
+            }
         }
+        var randChoiceElem = document.querySelector("#mysteryWord");
+        console.log('guessedWord ----', guessedWord.join(''))
+        randChoiceElem.innerHTML = guessedWord.join('');
+    }
+    function reset() {
+        guesses = [];
+        var guessesElem = document.querySelector("#lettersDone");
+        guessesElem.innerHTML = guesses;
+        lives = 7;
+        correctGuesses = [];
+        guessedWord = [];
+        //chooses random word
         //stores random word chosen
         var randChoice = randWord();
             console.log("randChoice", randChoice);
 
 
 
-        var wordArr = randChoice.split(""); 
+        wordArr = randChoice.split(""); 
 
         
         //Adds underscores to page
@@ -25,18 +52,29 @@
         
         var underscoreHTML = "";
         for (var i = 0; i < wordArr.length; i++){
-            underscoreHTML += "_ " ;
+            // underscoreHTML += "_ " ;
+            guessedWord.push('_ ')
         }
         var randChoiceElem = document.querySelector("#mysteryWord");
         document.querySelector("#mysteryWord").innerHTML = underscoreHTML;
-
+    }
+        
+    reset()
 
         var startGame = function (event){
+            var playerGuess = event.key;
 
+            var indices = [];
+            for(var i=0; i<wordArr.length;i++) {
+                if (wordArr[i] === playerGuess) {
+                    indices.push(i);
+                }
+            }
+            console.log('all the spots where we found the guessed letter!!!', indices);
+            checkWord(playerGuess)
             //while(lives > 0 && !hasWon){ //shorthand for !== true
                 // hasMatch = false;
                 // var playerGuess = event.key;
-                var playerGuess = event.key;
                 console.log("You guessed", playerGuess);
                 guesses.push(playerGuess);
                 console.log("guesses", guesses);
@@ -52,22 +90,23 @@
                 }else{
                     correctGuesses.push(playerGuess);
                     console.log(correctGuesses);
-                        var correctLetterElem = document.querySelector("#mysteryWord");
-                        document.querySelector("#mysteryWord").innerHTML = correctGuesses;
+                        // var correctLetterElem = document.querySelector("#mysteryWord");
+                        // document.querySelector("#mysteryWord").innerHTML = correctGuesses;
 
                 }
                 var hasWon = winner(guesses, wordArr);
                     console.log("win?", hasWon);
                 
                 if(hasWon){
+                    alert('you did it!!')
                     wins++;
-                    alert("you did it!");
     
                     var winsElem = document.querySelector("#wins");
                     winsElem.innerHTML = wins;
+                    reset()
 
                 }
-                startGame();
+                // startGame();
 
                 //create reset button 
                 // var randChoice = randWord();
@@ -97,7 +136,7 @@
 
         document.addEventListener("keyup", startGame);
         //$("body").on("keyup", startGame);
-        startGame();
+        // startGame();
        
 
         console.log("player stats", player);
